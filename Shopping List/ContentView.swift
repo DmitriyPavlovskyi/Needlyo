ad3917a4c77eca28a355ddf158312d9ae4a2a25f
Dmytro Pavlovskyi <dmytro.pavlovskyi@abstract.it>
2026-07-04 08:59:44 +0100

    Initial Commit

---
+++ Shopping List/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Shopping List
+//
+//  Created by Dmytro Pavlovskyi1 on 04.07.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Shopping List/Shopping_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shopping_ListApp.swift
+//  Shopping List
+//
+//  Created by Dmytro Pavlovskyi1 on 04.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct Shopping_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
